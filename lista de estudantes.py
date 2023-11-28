5d167d4e67eef8eebaecc76aa765cfa05239317a
LeonardoKelmy <leonardosenaijacob@gmail.com>
2023-11-28 16:36:18 -0300

    commit inicial 0.3

---
+++ lista de estudantes.py
@@ -0,0 +1,30 @@
+#Lista de estudantes- Gerenciamento de dados
+
+#Aprsentação
+print('\n\t\t\t -- Gerenciamento de dados --\n')
+# aqui você irá conseguir os dados dos alunos de uma classe
+
+
+ #Entradas
+escola = input('informe o nome da intituição: ')
+aluno = input('informe o ra: ')
+nome = input('informe o nome do aluno: ')
+
+
+ #Processamento
+kamila = ('nome: Kamila Nobre','Nota: 10','Curso: Enfermagem')
+
+ #saida
+
+if nome == 'kamila':
+     print(f'{kamila}')
+
+
+
+
+
+
+
+
+
+
